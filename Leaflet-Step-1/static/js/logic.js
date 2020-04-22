0e41d15159348c4c47332d0c98fafc3e353dd29b
sagastya <sagastya@yahoo.com>
2020-04-22 03:51:42 -0400

    added map style

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,14 @@
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Creating initial map object
+var myMap = L.map("map", {
+    center: [38.28386,-98.33500],
+    zoom: 5
+});
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets-satellite",
+    accessToken: API_KEY
+}).addTo(myMap);
