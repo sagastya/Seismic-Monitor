6f1dbbe898f08a81f164442cec31756ff912de49
sagastya <sagastya@yahoo.com>
2020-05-03 00:08:48 -0400

    changed layer logic

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -15,111 +15,77 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 
 d3.json(queryUrl, function (data) {
     // console.log(data.features);
-    console.log(data);
-    var geojson;
-
-    function markerSize(magnitude) {
-        return parseInt(magnitude) * 5;
-    };
 
     // Create marker options
     var geojsonMarkerOptions = {
         radius: 8,
-        color: "#000",
+        color: "000",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.5
     };
 
-    // Create a new choropleth layer
-    geojson = L.choropleth(data, {
+    function getColor(d) {
+        return d > 5 ? '#FF8C00' :
+               d > 4 ? '#CFA309' :
+               d > 3 ? '#DEBA13' :
+               d > 2 ? '#CED11C' :
+               d > 1 ? '#BDE826' :
+                       '#ADFF2F';
 
-        // Define what property in the features to use
-        valueProperty: "mag",
+    };
 
-        // Set color scale
-        scale: ["#adff2f", "#ff8c00"],
+    function getRadius(d) {
+        return d > 5 ? 20 :
+               d > 4 ? 18 :
+               d > 3 ? 15:
+               d > 2 ? 12 :
+               d > 1 ? 8 :
+                       5;
+    };
 
-        // Number of breaks in step range
-        steps: 6,
+    function style(feature) {
+        return {
+            fillColor: getColor(feature.properties.mag),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            fillOpacity: 0.7,
+            radius: getRadius(feature.properties.mag)
+        };
+    };
 
-        // q for quartile, e for equidistant, k for k-means
-        mode: "q",
-        style: {
-            // Border color
-            color: "#a9a9a9",
-            weight: 1,
-            fillOpacity: 0.5
-        },
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + "Magnitude: " + feature.properties.mag +
+        "</p><p>" + new Date(feature.properties.time) + "</p>");
+    };
 
-        // Binding a pop-up to each layer
-        onEachFeature: function (feature, layer) {
-            layer.bindPopup("<h3>" + feature.properties.place +
-                "</h3><hr><p>" + "Magnitude: " + feature.properties.mag +
-                "</p><p>" + new Date(feature.properties.time) + "</p>");
-        },
+
+    L.geoJSON(data.features, {
+        onEachFeature: onEachFeature,
+        style: style,
         pointToLayer: function (feature, latlng) {
-
             return L.circleMarker(latlng, geojsonMarkerOptions);
         }
     }).addTo(myMap);
+
 
     // Set up the legend
     var legend = L.control({ position: "bottomright" });
     legend.onAdd = function () {
         var div = L.DomUtil.create('div', 'info legend');
-        var colors = geojson.options.colors;
+        var colors = ['#ADFF2F', '#BDE826', '#CED11C', '#DEBA13', '#CFA309', '#FF8C00']
         var categories = ['0-1', '1-2', '2-3', '3-4', '4-5', '5+']
 
         for (var i = 0; i < categories.length; i++) {
             div.innerHTML +=
-                '<i style="background:' + colors[i] + '"></i> ' 
+                '<i style="background:' + colors[i] + '"></i> '
                 + categories[i] + '<br />';
         }
         return div;
     };
+
     // Adding legend to the map
     legend.addTo(myMap);
-    
-    // createMap(geojson, legend);
 });
-
-function createMap(earthquakes, legend) {
-
-    // Define lightmap and satellitemap layers
-    var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "mapbox.light",
-        accessToken: API_KEY
-    });
-
-    var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "mapbox.streets-satellite",
-        accessToken: API_KEY
-    });
-
-    // Define a baseMaps object to hold our base layers
-    var baseMaps = {
-        "Grayscale": lightmap,
-        "Satellite": satellitemap
-    };
-
-    // Create overlay object to hold our overlay layer
-    var overlayMaps = {
-        Earthquakes: earthquakes
-    };
-
-    // Creating initial map object
-    var myMap = L.map("map", {
-        center: [37.09, -95.71],
-        zoom: 5,
-        layers: [lightmap, earthquakes]
-    });
-    legend.addTo(myMap);
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(myMap);
-}
